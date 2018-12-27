c73c90c317e011d0c9cddb90b0c7684d5f1cb53e
Wendy Calderon <wendycalderon@gmail.com>
2018-12-27 06:10:38 +0000

    Create figures/new form

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,10 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  get '/figures' do
+    @figure = Figure.all
+    erb :'figures/index' 
+  end
+  
+  get '/figures/new' do 
+    erb :'/figures/new'
+  end
 end
