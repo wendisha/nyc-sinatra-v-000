7b189481520656bd4b1f0fa1b0c51a6e0ac5da64
Wendy Calderon <wendycalderon@gmail.com>
2018-12-28 02:38:46 +0000

    Finish figures/new form and cont actions

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -7,4 +7,16 @@ class FiguresController < ApplicationController
   get '/figures/new' do 
     erb :'/figures/new'
   end
+  
+  post '/figures' do 
+    @figure = Figure.create(params[:figure])
+    if !params["title"]["name"].empty?
+      @figure.titles << Title.create(name: params["title"]["name"])
+    end
+    if !params["landmark"]["name"].empty?
+      @figure.landmarks << Landmark.create(name: params["landmark"]["name"])
+    end
+    @figure.save
+    redirect "figures/#{@figure.id}"
+  end
 end
