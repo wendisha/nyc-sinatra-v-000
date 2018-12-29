be818e80e98ae50004f916bac2ec8489a8649d74
Wendy Calderon <wendycalderon@gmail.com>
2018-12-29 02:29:43 +0000

    Allow user to edit a single landmark

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -12,4 +12,21 @@ class LandmarksController < ApplicationController
     @landmark = Landmark.create(params[:landmark])
     redirect "landmarks/#{@landmark.id}"
   end
+  
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :"landmarks/show"
+  end
+  
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :'/landmarks/edit'
+  end
+  
+    patch '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    @landmark.update(params[:landmark])
+    @landmark.save
+    redirect to("/landmarks/#{@landmark.id}")
+   end
 end
